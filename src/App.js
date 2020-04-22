9f9484c1f6a8d86fca365ebd36803164ef8646ea
AvetisDN <avetisdn@gmail.com>
2020-04-22 17:26:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React, { Component } from 'react';
+import CreatePost from './containers/CreatePost';
+import PostList from './containers/PostList';
+//import NewPost from './components/NewPost';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container my-3">
+        <div className="row">
+          <div className="col-md-6">
+            <CreatePost />
+          </div>
+          <div className="col-md-6">
+            <PostList />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
